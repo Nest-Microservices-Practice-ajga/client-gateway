c6c720ddbda8cded9190f24d7a5c1e44666d2d8e
Alejandro Guevara <alejandroguevara@alejandros-mbp.speedport.ip>
2024-09-14 18:57:56 +0200

    progress adding orders microservice

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,60 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Inject,
+  Query,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { ORDER_SERVICE } from 'src/config';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
+import { OrderPaginationDto, StatusOrderDto } from './dto';
+import { PaginationDto } from 'src/common';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(
+    @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy,
+  ) {}
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    return this.ordersClient.send('createOrder', createOrderDto);
+  }
+
+  @Get()
+  findAll(@Query() paginationDto: OrderPaginationDto) {
+    return this.ordersClient.send('findAllOrders', paginationDto);
+  }
+
+  @Get('id/:id')
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    try {
+      const order = await firstValueFrom(
+        this.ordersClient.send('findOneOrder', { id }),
+      );
+      return order;
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+
+  @Get(':status')
+  async findAllByStatus(
+    @Param() statusDto: StatusOrderDto,
+    @Query() paginationDto: PaginationDto,
+  ) {
+    try {
+      return this.ordersClient.send('findAllOrders', {
+        ...paginationDto,
+        status: statusDto.status,
+      });
+    } catch (error) {
+      throw new RpcException(error);
+    }
+  }
+}
